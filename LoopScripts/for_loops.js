efdccd55dd2b9e9241003a5596a2cff9a9e1f82d
Richard Perez <richard.floresperez0@gmail.com>
2022-10-31 15:31:55 -0400

    Loop exercises

---
+++ LoopScripts/for_loops.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// for (let i = 0; i < 7; i++) {
+//     console.log("I love loops");
+// }
+
+let x;
+for(x = 1;x <= 7; x++){
+    console.log("I like loops");
+}
---
+++ LoopScripts/while_loops.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// let i = 0;
+// while (i < 7) {
+//     console.log("I love loops");
+//     i++;
+// }
+
+let x = 1;
+do{
+     console.log("I like loops");
+     x++;
+
+} while(x <= 7)
